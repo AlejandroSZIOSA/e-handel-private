07076f931364c4caff756b4e85937832977e7f24
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-09-03 20:57:29 +0200

    Some Changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <h1>Home page</h1>
       <p>
         <Link href="/product-details"> To the products</Link>
