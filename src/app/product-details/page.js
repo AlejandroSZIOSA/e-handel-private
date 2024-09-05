b917332287103b36965098eb3ec02e70bf17162e
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-09-05 19:13:11 +0200

    Some Changes

--- src/app/product-details/page.js
+++ src/app/product-details/page.js
@@ -5,7 +5,9 @@ import Link from "next/link";
 //2-retrieve Obj from Home Page
 
 export default function ProductsDetailsPage({ searchParams }) {
+  const { totalProducts } = searchParams; //Destructuring
   console.log(searchParams);
+
   return (
     <main>
       <h1>Product details</h1>
@@ -15,8 +17,8 @@ export default function ProductsDetailsPage({ searchParams }) {
       <p>
         <Link href={`product-details/${2}`}> To product 2</Link>
       </p>
-      <h2>Retrive Data</h2>
-      <p>{}</p>
+      <h2>Retrieve Data</h2>
+      <p>{totalProducts}</p>
     </main>
   );
 }
