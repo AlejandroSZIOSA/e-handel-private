e9eaa75d1dc38c25936d3bba43cbadd5374ee4c0
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-09-05 17:13:16 +0200

    Some changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,23 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const [totalQuantityProducts, setTotalQuantityProducts] = useState(0);
+
   return (
     <main>
       <h1>Home Page</h1>
       <p>
-        <Link href="/product-details"> To the products</Link>
+        <Link href="/product-details"> Link To product Detail Page</Link>
       </p>
+      <h2>Increase Total product</h2>
+      <p>Total Products:{totalQuantityProducts}</p>
+      <button
+        onClick={(e) => setTotalQuantityProducts(totalQuantityProducts + 1)}
+      >
+        Increase Total products
+      </button>
     </main>
   );
 }
--- src/app/product-details/page.js
+++ src/app/product-details/page.js
@@ -1,12 +1,16 @@
+"use client";
 import Link from "next/link";
 
 //1-Dynamic Params Here
-export default function ProductsDetailsPage() {
+export default function ProductsDetailsPage(params) {
   return (
     <main>
       <h1>Product details</h1>
       <p>
         <Link href={`product-details/${1}`}> To product 1</Link>
+      </p>
+      <p>
+        <Link href={`product-details/${2}`}> To product 2</Link>
       </p>
     </main>
   );
